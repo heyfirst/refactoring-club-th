53f5f84200f70b9e47341d65a85e07431ef50e3a
Kanisorn Sutham <firstziiz.k@gmail.com>
2021-01-30 13:40:47 +0700

    refactor from function to object fizzbuzz

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,11 +1,12 @@
-def fizzbuzz(num: int):
-    if num % 3 == 0 and num % 5 == 0:
-        return "fizzbuzz"
+class FizzBuzz:
+    def say(self, num: int):
+        if num % 15 == 0:
+            return "fizzbuzz"
 
-    if num % 3 == 0:
-        return "fizz"
+        if num % 3 == 0:
+            return "fizz"
 
-    if num % 5 == 0:
-        return "buzz"
+        if num % 5 == 0:
+            return "buzz"
 
-    return num
+        return num
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,23 +1,27 @@
-from unittest import TestCase
-from fizzbuzz import fizzbuzz
+from unittest import TestCase, main
+from fizzbuzz import FizzBuzz
 
 
 class TestFizzBuzz(TestCase):
-    def test_always_true(self):
-        self.assertEqual(True, True)
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
 
     def test_should_return_number_when_inputs_are_not_multiplied_by_3_and_5(self):
         for number in [1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19]:
-            self.assertEqual(fizzbuzz(number), number)
+            self.assertEqual(self.fizzbuzz.say(number), number)
 
     def test_should_return_fizz_when_inputs_are_multiplied_by_3(self):
         for number in [3, 6, 9, 12, 18]:
-            self.assertEqual(fizzbuzz(number), "fizz")
+            self.assertEqual(self.fizzbuzz.say(number), "fizz")
 
     def test_should_return_buzz_when_inputs_are_multiplied_by_5(self):
         for number in [5, 10, 20]:
-            self.assertEqual(fizzbuzz(number), "buzz")
+            self.assertEqual(self.fizzbuzz.say(number), "buzz")
 
     def test_should_return_buzz_when_inputs_are_multiplied_by_3_and_5(self):
         for number in [15, 30]:
-            self.assertEqual(fizzbuzz(number), "fizzbuzz")
+            self.assertEqual(self.fizzbuzz.say(number), "fizzbuzz")
+
+
+if __name__ == "__main__":
+    main()
